b7149891762db693ca4c0c0f96b1425dac485575
leaBroe <lea.broennimann@students.unibe.ch>
2023-05-10 23:04:48 +0200

    feat: added CNN class and train function

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 """Main python script, used to build the model from scratch
 """
 
-
 import scripts.preprocessing as preprocessing
-
+import scripts.training as train
+import torch
 
 # Load the data, transform it
-train_data, test_data, train_loader, test_loader = preprocessing.load(32, 1)
+train_data, test_data, train_loader, test_loader = preprocessing.load(32, 0)
 
 
 # Checking dataset size and visualizing a few images
 preprocessing.data_check(train_data, test_data, train_loader)
+
+# Create CNN
+net = train.CNN()
+
+# train model
+epochs = 10
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+train_model = train.train(net, train_loader, epochs, device)
+
---
+++ scripts/training.py
@@ -0,0 +1,56 @@
+import torch.nn as nn
+import torch.optim as optim
+
+#  create a class that inherits from the nn.Module class
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3)
+        self.fc1 = nn.Linear(in_features=64 * 5 * 5, out_features=128)
+        self.fc2 = nn.Linear(in_features=128, out_features=10)
+
+    def forward(self, x):
+        x = nn.functional.relu(self.conv1(x))
+        x = nn.functional.max_pool2d(x, 2)
+        x = nn.functional.relu(self.conv2(x))
+        x = nn.functional.max_pool2d(x, 2)
+        x = x.view(-1, 64 * 5 * 5)
+        x = nn.functional.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+
+
+
+def train(net, trainloader, epochs, device):
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+    net.to(device)
+
+    for epoch in range(epochs):  # loop over the dataset multiple times
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
+            inputs, labels = inputs.to(device), labels.to(device)
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:    # print every 2000 mini-batches
+                print('[%d, %5d] loss: %.3f' %
+                      (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
+
+    print('Finished Training')
+
