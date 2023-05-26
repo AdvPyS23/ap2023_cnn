71bd275918969383fe21a172a4943234812d0e23
MansFaye <mohamedmansour.faye@unifr.ch>
2023-05-26 16:26:42 +0200

    feat: added validation in the training function

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import torch
 # Load the data, transform it
 batch_size = 32
 # set cores to 0?
-train_data, test_data, train_loader, test_loader = preprocessing.load(batch_size, 0)
+train_data, test_data, train_loader, test_loader = preprocessing.load(batch_size, 6)
 
 
 # Checking dataset size and visualizing a few images
@@ -20,5 +20,5 @@ net = train.CNN()
 # train model
 epochs = 5
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-train_model = train.train(net, train_loader, epochs, device)
+train_model = train.train(net, train_loader, test_loader, epochs, device)
 
