c5a5e7d3d0feb79dc049d6cba553c1356e80efdf
MansFaye <mohamedmansour.faye@unifr.ch>
2023-05-31 17:55:21 +0200

    feat: full loss plotting function

--- scripts/visualization.py
+++ scripts/visualization.py
@@ -46,17 +46,25 @@ def loss_acc_plot(train_loss, test_loss, train_acc, test_acc, epochs):
     epochs
         Number of epochs the model was trained for
     """
-    epochs = 20
     steps_train = np.linspace(1, epochs + 1, 18 * epochs)
-    #steps_test = np.linspace(1, epochs + 1, ?)
+    steps_test = np.linspace(1, epochs + 1, 3 * epochs)
+
     # Initiate plotting
-    fig, axs = plt.subplots(1, 2)
-    axs[0, 0].plot(steps_train, train_loss, '-o')
-    axs[0, 0].set_xlabel('Epochs')
-    axs[0, 0].set_ylabel('Loss')
+    fig, axs = plt.subplots(1, 2, figsize=(20, 10))
+    axs[0].plot(steps_train, train_loss, '-', label='training')
+    axs[0].plot(steps_test, test_loss, '-', label='testing')
+    axs[0].set_xlabel('Epochs')
+    axs[0].set_ylabel('Loss')
+    axs[0].set_ylim(0, 1.5)
+    axs[0].legend(loc='upper right', frameon=False)
 
-    axs[0, 1].plot(steps_train, train_acc, '-o')
-    axs[0, 1].set_xlabel('Epochs')
-    axs[0, 1].set_ylabel('Accuracy (%)')
+    axs[1].plot(steps_train, train_acc, '-', label='training')
+    axs[1].plot(steps_test, test_acc, '-')
+    axs[1].set_xlabel('Epochs')
+    axs[1].set_ylabel('Accuracy (%)')
+    axs[1].set_ylim(40, 100)
+    axs[1].legend(loc='upper right', frameon=False)
 
+    # Save the figure
     plt.savefig('./data/acc_loss.png')
+    print("Image has been saved as ./data/acc_loss.png")
